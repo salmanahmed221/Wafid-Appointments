cb08601176955fcb0a603fe6d46e0f928224ec50
salmanahmed221 <salmanahmed121221@gmail.com>
2024-09-14 11:43:25 +0500

    change deploy link

--- src/constant/constants.ts
+++ src/constant/constants.ts
@@ -1,4 +1,4 @@
 export const BASE_URL =
   process.env.NODE_ENV == "development"
     ? "http://localhost:3000"
-    : "https://form-data-storing.vercel.app";
+    : "https://wafid-appointments.vercel.app";
